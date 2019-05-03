a9a0b54d0d2c3716b0924726d2c9646d865cf8af
Priya <priyabodke2@gmail.com>
2019-05-03 10:19:13 +0000

    commit

---
+++ Temp_Project/README.md
@@ -0,0 +1,8 @@
+## Temp_Project infrastructure
+
+Created 5/3/19 by admin. for Temp_Project
+
+
+## Design Decisions
+| Design item                | Decision|
+| :----------------------------------- | :--------------------------------------------------------------------------------|
---
+++ Temp_Project/main.tf
@@ -0,0 +1,38 @@
+#####################################################################
+##
+##      Created 5/3/19 by admin. for Temp_Project
+##
+#####################################################################
+
+terraform {
+  required_version = "> 0.8.0"
+}
+
+provider "google" {
+  project = "{{beaming-inn-237311 }}"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = "default"
+    access_config = {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
